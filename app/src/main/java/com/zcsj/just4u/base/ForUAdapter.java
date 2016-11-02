a13a1d7e4cddd35143f7fd95c6167a7204cb13d8
qianyue0317 <1974765766@qq.com>
2016-11-02 16:30:27 +0800

    继续补充开发文档,添加design依赖包

--- app/src/main/java/com/zcsj/just4u/base/ForUAdapter.java
+++ app/src/main/java/com/zcsj/just4u/base/ForUAdapter.java
@@ -1,32 +1,115 @@
 package com.zcsj.just4u.base;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import com.zcsj.just4u.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 适配器基类
  * Created by abc on 2016/11/2.
  */
 
-public class ForUAdapter extends BaseAdapter {
-    @Override
-    public int getCount() {
-        return 0;
+public abstract class ForUAdapter<T> extends BaseAdapter {
+
+    protected List<T> data;
+    private int layoutId;
+
+    public ForUAdapter(List<T> data, int layoutId) {
+        if (data == null) {
+            this.data = new ArrayList<>();
+        } else {
+            this.data = data;
+        }
+        this.layoutId = layoutId;
+    }
+
+    /**
+     * 更新数据
+     *
+     * @param data
+     */
+    public void updateData(List<T> data) {
+        if (data != null) {
+            this.data.clear();
+            this.data.addAll(data);
+            notifyDataSetChanged();
+        }
+    }
+
+
+    /**
+     * 添加数据
+     *
+     * @param data
+     */
+    public void addData(List<T> data) {
+        if (data != null) {
+            this.data.addAll(data);
+            notifyDataSetChanged();
+        }
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public int getCount() {
+        return this.data != null ? this.data.size() : 0;
+    }
+
+    @Override
+    public T getItem(int position) {
+        return data.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(layoutId, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        bindData(viewHolder,data.get(position),position);
+
+        return convertView;
     }
+
+    public abstract void bindData(ViewHolder viewHolder,T t, int position);
+
+
+    public class ViewHolder{
+        Map<Integer,View> views;
+        View itemView;
+        ViewHolder(View itemView) {
+            views = new HashMap<>();
+            this.itemView = itemView;
+        }
+
+        public View getView(int id) {
+            View view;
+            if (views.containsKey(id)) {
+                view = views.get(id);
+            } else {
+                view = itemView.findViewById(id);
+                views.put(id, view);
+            }
+            return view;
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/zcsj/just4u/view/viewImpl/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.zcsj.just4u.view.viewImpl;
+
+import com.zcsj.just4u.base.BaseActivity;
+
+/**
+ * 欢迎页面,做的炫一点,一个花开的逐帧动画
+ * Created by abc on 2016/11/2.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+
+
+}
