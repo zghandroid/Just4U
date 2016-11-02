167056356fb93e60390a86e8bceae483c48f3f40
qianyue0317 <1974765766@qq.com>
2016-11-02 16:43:34 +0800

    继续补充开发文档,小部分修改

--- app/src/main/java/com/zcsj/just4u/view/viewImpl/SplashActivity.java
+++ app/src/main/java/com/zcsj/just4u/view/viewImpl/SplashActivity.java
@@ -1,14 +1,19 @@
 package com.zcsj.just4u.view.viewImpl;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.zcsj.just4u.base.BaseActivity;
 
 /**
- * 欢迎页面,做的炫一点,一个花开的逐帧动画
- * Created by abc on 2016/11/2.
+ * 欢迎页面,做的炫一点,一个玫瑰花开的逐帧动画
+ * Created by qy on 2016/11/2.
  */
 
 public class SplashActivity extends BaseActivity {
 
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
